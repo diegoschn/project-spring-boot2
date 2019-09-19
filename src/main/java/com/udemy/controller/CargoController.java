946e9b4733470d4739084e87230eaeb3ac50a06d
diegoschn <diegoschneiderssa@gmail.com>
2019-09-19 07:07:57 -0300

    Implementando cadastro de cargo e edição

--- src/main/java/com/udemy/controller/CargoController.java
+++ src/main/java/com/udemy/controller/CargoController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -31,7 +33,8 @@ public class CargoController {
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.listarTodos());
 		return "/cargo/lista";
 	}
 	
@@ -45,5 +48,18 @@ public class CargoController {
 	@ModelAttribute("departamentos")
 	public List<Departamento> getDepartamentos(){
 		return departamentoService.listarTodos();
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Integer id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "/cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes redirect) {
+		this.cargoService.editar(cargo);
+		redirect.addFlashAttribute("success","Cargo atualizado com sucesso!");
+		return "redirect:/cargos/listar";
 	}
 }
